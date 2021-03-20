8111bb24ad486bfe30b680cf664a58044a6975da
Thomas Girard <54374885+dalton-x@users.noreply.github.com>
2021-03-20 11:29:50 +0100

    Ajout du Get pour le fullName directement sur les templates

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -90,6 +90,11 @@ class User implements UserInterface
         return $this;
     }
 
+    public function getFullName(): ?string
+    {
+        return $this->firstName .' '. $this->lastName;
+    }
+
     public function getEmail(): ?string
     {
         return $this->email;
