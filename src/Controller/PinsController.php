232022fce1f3b7662dc3bbd7e40f5801cf182255
Thomas Girard <54374885+dalton-x@users.noreply.github.com>
2021-03-26 18:56:31 +0100

    Ajout de l'authentification système

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Pin;
 use App\Form\PinType;
 use App\Repository\PinRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,7 +27,7 @@ class PinsController extends AbstractController
         /**
      * @Route("/pins/create", name="app_pins_create", methods="GET|POST")
      */
-    public function create(Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepo): Response
         {
             // initilisateion de l'objet Pin
             $pin = new Pin;
@@ -36,6 +37,8 @@ class PinsController extends AbstractController
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
+                $user = $userRepo->findOneBy(['email' => 'test@gmail.com']);
+                $pin->setUser($user);
                 $em->persist($pin);
                 $em->flush();
 
